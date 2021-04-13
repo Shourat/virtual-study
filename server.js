0f1832b6322f0edc40c1552086cec66c3b9190ff
Shourat <Shourat123@gmail.com>
2021-04-13 17:13:05 -0400

    backend working

---
+++ server.js
@@ -0,0 +1,37 @@
+
+//backend with node js
+
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const home = fs.readFileSync('nodeindex.html')
+const discussion = fs.readFileSync('./discussion.html')
+const flashcards = fs.readFileSync('./flashcards.html')
+
+const server = http.createServer((req, res)=>{
+    console.log(req.url);
+    url = req.url;
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    if(url == '/'){
+        res.end(home);
+    }
+    else if(url == '/discussion'){
+        res.end(discussion);
+    }
+    else if(url == '/flashcards'){
+        res.end(flashcards);
+    }
+    
+    else{
+        res.statusCode = 404;
+        res.end("<h1>404 not found</h1>");
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
